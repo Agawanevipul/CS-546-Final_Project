35ee2df456827976da1f47adaa7bc9846318b801
Agawanevipul <vipulagawane9@gmail.com>
2023-04-04 00:59:21 -0400

    handle-bar nav and footer

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const constructorMethod = (app) => {
   app.use("/", (req, res) => {
     res.render("foo", {});
   });
+  // app.get("/", (req, res) => {
+  //   res.sendFile(path.resolve("login/index.html"));
+  // });
 
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
