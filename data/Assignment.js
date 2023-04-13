bf0304436a41dbbb842984eef3cc192ad779cf92
Ayushimalaviya <ayushimalaviya1999@gmail.com>
2023-04-13 15:54:57 -0400

    Validations on Route and db

--- data/Assignment.js
+++ data/Assignment.js
@@ -1,14 +1,25 @@
 import { studentCollection } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
-import validator from '../validator.js';
+import validator from './validator.js';
 const method = {
   async create(firstName,lastName,emailId,CWID,program,major,password,confirmPassword) {
+    firstName = validator.checkString(firstName, 'First Name');
+    lastName = validator.checkString(lastName, 'Last Name');
+    emailId = validator.checkString(emailId, 'Email Id');
+    emailId = validator.validateEmailId(emailId);
+    program = validator.checkString(program, 'Program');
+    major = validator.checkString(major, 'Major');
+    password = validator.checkString(password, 'Password');
+    password = validator.validatePassword(password);
+    confirmPassword = validator.checkString(confirmPassword, 'Confirm Password')
+    confirmPassword = validator.validatePassword(confirmPassword);
+
     const salt = await bcrypt.genSalt(10);
     const passwords = await bcrypt.hash(password, salt);
     const confirmPasswords = await bcrypt.hash(confirmPassword, salt);
     const student_id = new ObjectId();
-    
+
     let obj1 = {
       _id: student_id,
       student_id,
@@ -20,7 +31,6 @@ const method = {
       major: major,
       password:passwords,
       confirmPassword:confirmPasswords
-      // year_Completion: year_Completion,
     };
     const infoCollection = await studentCollection();
     const insertInfo = await infoCollection.insertOne(obj1);
