7bc64d544b15c4c98638a058e877103f3c7905ce
Agawanevipul <vipulagawane9@gmail.com>
2023-05-05 14:22:34 -0400

    routes

--- data/index.js
+++ data/index.js
@@ -1,9 +1,10 @@
 import students from "./studentModel.js";
 import courses from "./courseModel.js";
 import description from "./description.js";
+import assignments from "./assignments.js";
 
 const studentsInfo = students;
 const courseInfo = courses;
 const descriptionInfo = description;
-
-export {studentsInfo, courseInfo, descriptionInfo}
+const assignmentInfo = assignments;
+export { studentsInfo, courseInfo, descriptionInfo, assignmentInfo };
--- routes/routesModels.js
+++ routes/routesModels.js
@@ -1,126 +1,150 @@
 import { Router } from "express";
 import path from "path";
-import { studentsInfo } from "../data/index.js";
+import { studentsInfo, assignmentInfo, courseInfo } from "../data/index.js";
 import { title } from "process";
 import validator from "../validator.js";
 
 const router = Router();
-router
-  .route("/")
-  // .get(async (req, res) => {
-  //   //code here for GET
-  //   try{
-  //     res.render('register', {title: "Register"});
-  //   } catch (e) {
-  //     res.status(404).json({error: e});
-  //   }
+router.route("/").post(async (req, res) => {
+  let isclicked = req.body.clicked;
+  console.log(isclicked);
+  if (isclicked === "registered") {
+    let firstName = req.body.first_name;
+    let lastName = req.body.last_name;
+    let emailId = req.body.email_id;
+    let CWID = req.body.cwid;
+    let program = req.body.program;
+    let major = req.body.major;
+    let password = req.body.create_pass;
+    let confirmPassword = req.body.confirm_pass;
+    try {
+      firstName = validator.checkString(firstName, "First Name");
+      lastName = validator.checkString(lastName, "Last Name");
+      emailId = validator.checkString(emailId, "Email Id");
+      emailId = validator.validateEmailId(emailId);
+      program = validator.checkString(program, "Program");
+      major = validator.checkString(major, "Major");
+      password = validator.checkString(password, "Password");
+      password = validator.validPassword(password);
+      confirmPassword = validator.checkString(
+        confirmPassword,
+        "Confirm Password"
+      );
+      confirmPassword = validator.validPassword(confirmPassword);
 
-  // })
-  .post(async (req, res) => {
-    let isclicked = req.body.clicked;
-    console.log(isclicked);
-    if (isclicked === "registered") {
-      let firstName = req.body.first_name;
-      let lastName = req.body.last_name;
-      let emailId = req.body.email_id;
-      let CWID = req.body.cwid;
-      let program = req.body.program;
-      let major = req.body.major;
-      let password = req.body.create_pass;
-      let confirmPassword = req.body.confirm_pass;
-      try {
-        firstName = validator.checkString(firstName, "First Name");
-        lastName = validator.checkString(lastName, "Last Name");
-        emailId = validator.checkString(emailId, "Email Id");
-        emailId = validator.validateEmailId(emailId);
-        program = validator.checkString(program, "Program");
-        major = validator.checkString(major, "Major");
-        password = validator.checkString(password, "Password");
-        password = validator.validatePassword(password);
-        confirmPassword = validator.checkString(
-          confirmPassword,
-          "Confirm Password"
+      if (password === confirmPassword) {
+        const student = await studentsInfo.create(
+          firstName,
+          lastName,
+          emailId,
+          CWID,
+          program,
+          major,
+          password,
+          confirmPassword
         );
-        confirmPassword = validator.validatePassword(confirmPassword);
-
-        if (password === confirmPassword) {
-          const student = await studentsInfo.create(
-            firstName,
-            lastName,
-            emailId,
-            CWID,
-            program,
-            major,
-            password,
-            confirmPassword
-          );
-          res.json(student);
-        } else {
-          const error = "Confirm password must be similar to password.";
-          return res.status(400).render("register", {
-            title: "Register",
-            loginPage: false,
-            firstName: firstName,
-            lastName: lastName,
-            CWID: CWID,
-            emailId: emailId,
-            program: program,
-            major: major,
-            error,
-          });
-        }
-      } catch (e) {
-        res.status(404).render("register", {
+        res.json(student);
+      } else {
+        const error = "Confirm password must be similar to password.";
+        return res.status(400).render("register", {
           title: "Register",
           loginPage: false,
           firstName: firstName,
           lastName: lastName,
-          emailId: emailId,
           CWID: CWID,
+          emailId: emailId,
           program: program,
           major: major,
-          password: password,
-          confirmPassword: confirmPassword,
-          error: e,
+          error,
         });
-        return;
       }
+    } catch (e) {
+      res.status(404).render("register", {
+        title: "Register",
+        loginPage: false,
+        firstName: firstName,
+        lastName: lastName,
+        emailId: emailId,
+        CWID: CWID,
+        program: program,
+        major: major,
+        password: password,
+        confirmPassword: confirmPassword,
+        error: e,
+      });
+      return;
     }
-    if (isclicked === "loggedIn") {
-      let emailId = req.body.emailId;
-      let password = req.body.password;
-      try {
-        emailId = validator.checkString(emailId, "Email Id");
-        emailId = validator.validateEmailId(emailId);
-        password = validator.checkString(password, "Password");
+  }
+  if (isclicked === "loggedIn") {
+    let emailId = req.body.emailId;
+    let password = req.body.password;
+    try {
+      emailId = validator.checkString(emailId, "Email Id");
+      emailId = validator.validateEmailId(emailId);
+      password = validator.checkString(password, "Password");
 
-        const student = await studentsInfo.get_details(emailId, password);
-        res.json(student);
-      } catch (e) {
-        res.status(404).render("register", {
-          title: "Login",
-          loginPage: true,
-          emailId: emailId,
-          error: e,
-        });
-        return;
-      }
+      const student = await studentsInfo.get_details(emailId, password);
+      res.json(student);
+    } catch (e) {
+      res.status(404).render("register", {
+        title: "Login",
+        loginPage: true,
+        emailId: emailId,
+        error: e,
+      });
+      return;
     }
-  });
+  }
+});
+router
+  .route("/homepage")
+  .get(async (req, res) => {
+    const studentId = user.session.student_id;
 
-// router.route('/details').post(async (req, res) => {
-//   //code here for POST
-//   // const loginId = req.body.exampleInputEmail1;
-//   // const loginPassword=req.body.exampleInputPassword1;
-//   try {
-//     res.render('foo',{})
-//     // console.log(loginId,loginPassword)
-//     // res.render('foo',{loginId, loginPassword})
+    try {
+      studentId = validator.checkId(studentId, "studentId");
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+    try {
+      const courses = await courseInfo.get(studentId);
+      const assignments = await assignmentInfo.get(studentId);
+      res.json(post);
+    } catch (e) {
+      res.status(404).json({ error: e });
+    }
+  })
+  .post(async (req, res) => {});
+router.route("/profile").post(async (req, res) => {
+  let { student_id, semester, totalCourses, courseNames } = req.body;
 
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: "Internal server error" });
-//   }
-// });
+  if (!userInfo || Object.keys(userInfo).length === 0) {
+    return res
+      .status(400)
+      .json({ error: "There are no fields in the request body" });
+  }
+
+  try {
+    userInfo.semester = validator.checkNumber(userInfo.semester, "semester");
+    userInfo.totalCourses = validator.checkNumber(
+      userInfo.totalCourses,
+      "totalCourses"
+    );
+    userInfo.courseNames = validator.checkStringArray(
+      userInfo.courseNames,
+      "courseNames"
+    );
+    userInfo.student_id = validator.checkId(userInfo.student_id, "student_id");
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+  const course = { student_id, semester, totalCourses, courseNames };
+  try {
+    const newCourse = await courseInfo.create(course);
+    res.json(newCourse);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
 
 export default router;
